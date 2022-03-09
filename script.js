9a525e81a71cff1712f8acfbc3f269f7bc4a7b27
Bryan`Y. Limz <superuser@brylimz.com>
2022-03-09 11:02:28 +0700

    Enter

--- script.js
+++ script.js
@@ -194,3 +194,12 @@ console.log(typeof NaN)
 console.log(String(23), 23);
 
 // type coercion
+console.log(' I am '+ 23 + ' years old')
+console.log('23' - '10' - 3)
+console.log('23' / '2')
+console.log('23' > '18')
+
+let n = '1' + 1 //'11'
+n = n - 1;
+
+console.log(n)
