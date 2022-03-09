6852d5e3be45f1f5cacbda811641d29cbc848f77
Bryan`Y. Limz <superuser@brylimz.com>
2022-03-09 07:51:49 +0700

    Enter

---
+++ script.js
@@ -0,0 +1,41 @@
+/* let js = 'amazing';
+console.log(40+8+23+10);
+
+console.log("james");
+console.log(23);
+
+let firstNamePerson = "byl";
+
+
+console.log(firstNamePerson);
+
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+if (myFirstJob === 'Programmer') {
+   alert(myCurrentJob = 'Nice')
+}
+
+
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun)
+
+console.log(typeof javascriptIsFun)
+console.log(typeof 'jone')
+
+
+javascriptIsFun = 'Yes'
+
+console.log(javascriptIsFun)
+
+let year;
+
+year = 3
+console.log(year)
+console.log(typeof year)
+
+console.log(typeof null)
+*/
+
