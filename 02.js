e0f1eee7b1e7a4b4941399bc5d4a4ca13cea18c2
Bryan`Y. Limz <superuser@brylimz.com>
2022-03-10 18:48:36 +0700

    nice

--- 02.js
+++ 02.js
@@ -165,17 +165,49 @@ checkWinner(scoreDoplhins, scoreKoalas)
 
 
 
- */
+ 
 
-const friend1 = 'Michael';
-const friend2 = 'Steven';
-const friend3 = 'Peter';
+// const friend1 = 'Michael';
+// const friend2 = 'Steven';
+// const friend3 = 'Peter';
 
 const friends = ['Michael', 'Steven', 'Peter']
-console.log(friends)
+console.log(friends);
 
-const years = new Array(1991, 1984, 2022);
+const years = new Array(1991, 1984, 2008, 2020);
 console.log(years)
 
+console.log(friends[0]);
+console.log(friends[2]);
 
+console.log(friends.length);
+
+console.log(friends[friends.length -1]);
+
+friends[2] = 'Jay'
+console.log(friends)
+
+const firstName = 'Jonas'
+const jonas = [firstName, 'Schemdtmann', 2037 - 1991,
+'teacher', friends ];
+
+console.log(jonas)
+console.log(jonas.length);
+
+// Exercise
+const calcAge = function(birthYear) {
+    return 2037 - birthYear;
+}
+const newYears = [1990, 1967, 2002, 2010, 2018];
+
+
+const age1 = calcAge(newYears[0])
+const age2 = calcAge(newYears[1])
+const age3 = calcAge(newYears[newYears.length -3])
+console.log(age1, age2, age3)
+
+const ages = [calcAge(newYears[0]), calcAge(newYears[1])]
+console.log(ages)
+
+*/
 
