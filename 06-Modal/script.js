58a15b7644634edbf30ceb4cdf8829750be36f01
Bryan`Y. Limz <superuser@brylimz.com>
2022-03-14 19:20:54 +0700

    completed challenge 2

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -6,7 +6,6 @@ const btnOpenModal = document.querySelectorAll('.show-modal')
 console.log(btnOpenModal);
 
 const openModal = function(){
-    console.log('Button clicked');
     modal.classList.remove('hidden')
     overlay.classList.remove('hidden')
    
@@ -26,3 +25,11 @@ for (let i = 0; i < btnOpenModal.length; i++)
 btnCloseModal.addEventListener('click', closeModal)
 
 overlay.addEventListener('click', closeModal)
+
+document.addEventListener('keydown', function(e){
+console.log(e.key);
+
+if(e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal()
+    
+}})
