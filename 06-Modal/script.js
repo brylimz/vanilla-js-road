dd989a271c78b420f9e110fb8a2b838c5ecd4ab2
Bryan`Y. Limz <superuser@brylimz.com>
2022-03-14 18:40:24 +0700

    spread

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -5,28 +5,24 @@ const btnCloseModal = document.querySelector('.close-modal')
 const btnOpenModal = document.querySelectorAll('.show-modal')
 console.log(btnOpenModal);
 
-
-
-
-for (let i = 0; i < btnOpenModal.length; i++)
-    btnOpenModal[i].addEventListener('click', function(){
+const openModal = function(){
     console.log('Button clicked');
     modal.classList.remove('hidden')
     overlay.classList.remove('hidden')
    
-});
+};
 
-  
-btnCloseModal.addEventListener('click', function(){
+
+const closeModal = function(){
     modal.classList.add('hidden')
     overlay.classList.add('hidden')
+}  
 
-});
 
 
-overlay.addEventListener('click', function(){
-    modal.classList.add('hidden')
-    overlay.classList.add('hidden')
+for (let i = 0; i < btnOpenModal.length; i++)
+    btnOpenModal[i].addEventListener('click', openModal)
 
-});
+btnCloseModal.addEventListener('click', closeModal)
 
+overlay.addEventListener('click', closeModal)
