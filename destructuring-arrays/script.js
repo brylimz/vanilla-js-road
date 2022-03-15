319df3b117cc745e704dfced51244931d8d5d77c
Bryan`Y. Limz <superuser@brylimz.com>
2022-03-15 15:03:42 +0700

    well played

--- destructuring-arrays/script.js
+++ destructuring-arrays/script.js
@@ -11,7 +11,12 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  
 
+  order: function(starterIndex, mainIndex){
+ return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  }
+,
   openingHours: {
     thu: {
       open: 12,
@@ -27,3 +32,31 @@ const restaurant = {
     },
   },
 };
+
+
+const arr = [2,3,4]
+
+const [x,y,z] = arr
+console.log(x,y,z);
+
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+// console.log(main, secondary)
+
+[main, secondary] = [secondary, main]
+console.log(main, secondary)
+
+
+console.log(restaurant.order(2, 0))
+const [starter, mainCourse] = [restaurant.order(2, 0) ]
+console.log(starter, mainCourse)
+
+const nested = [ 2,5, [5,6]]
+// const [i,,j] = nested;
+// console.log(i, j)
+const [i,  ,[j, k]] = nested
+console.log(i, j, k)
