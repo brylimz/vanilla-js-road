b366c50789430d2c63a2c7a3cf9e4a35ec61b058
Bryan`Y. Limz <superuser@brylimz.com>
2022-03-16 10:11:36 +0700

    spread + rest operator

--- Data-Structures-Modern-Op/script.js
+++ Data-Structures-Modern-Op/script.js
@@ -110,5 +110,177 @@ console.log(i, j, k)
 const [p, q, r] = [8, 9 ]
 console.log(p,q,r)
 
+
+
+// SPREAD OPERATOR
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  
+
+  order: function(starterIndex, mainIndex){
+ return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+
+
+},
+
+orderDelivery: function({starterIndex = 1 , mainIndex = 0, time = '20:00', address}){
+console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`)
+
+
+}
+,
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+
+
+orderPasta: function(ing1, ing2, ing3){
+console.log(`Here is your delecition pasta with ${ing1}, ${ing2}, ${ing3}`)}
+};
+
+const arr = [7, 8, 9];
+const badNewArr = [1,2, arr[0], arr[1]]
+console.log(badNewArr)
+
+const newArr = [1, 2, ...arr];
+console.log(newArr)
+
+console.log([1, 2, ...arr])
+
+const newMenu = [...restaurant.mainMenu, 'gnocci'];
+console.log(newMenu)
+
+// Copy array
+const mainMenuCopy = {...restaurant.mainMenu};
+// join 2 arrays
+
+const menu = [...restaurant.mainMenu, ...restaurant.starterMenu]
+console.log(menu) 
+
+const str = 'jonas'
+const letters = [...str, '1']
+console.log(letters[0])
+
+// Real - world example
+// const ingredients = [prompt('let\'s make pasta! Ingredient 1?'), 
+// prompt('let\'s make pasta! Ingredient 2?'), 
+// prompt('let\'s make pasta! Ingredient 3?')]
+// console.log(ingredients)
+
+// restaurant.orderPasta(ingredients[0], ingredients[1], ingredients[2])
+
+// restaurant.orderPasta(...ingredients)
+
+// objects 
+const newRestaurant = {...restaurant, founder : 'Guze'}
+console.log(newRestaurant)
+
+const restaurantCopy = {...restaurant}
+restaurantCopy.name = 'Ristorante Roma';
+console.log(restaurantCopy)
+console.log(restaurant.name)
 */
 
+// REST OPERATOR
+
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  
+
+  order: function(starterIndex, mainIndex){
+ return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+
+
+},
+
+orderDelivery: function({starterIndex = 1 , mainIndex = 0, time = '20:00', address}){
+console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`)
+
+
+}
+,
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+
+
+
+  orderPasta: function(ing1, ing2, ing3){
+console.log(`Here is your delecition pasta with ${ing1}, ${ing2}, ${ing3}`)},
+
+orderPizza: function(mainIngredient, ...otherIngredients){
+  console.log(mainIngredient);
+  console.log(otherIngredients)
+}
+};
+
+
+// 1(destructuring)
+
+// SPREAD  because on right side of = 
+const arr = [1,2, ...[3,4]];
+console.log(arr)
+//REST, because on left side of = 
+
+const [a,b, ...others] = [1,2,3,4,5];
+console.log(a,b,others)
+
+const [pizza, , Risotto, ...otherfood]  = [...restaurant.mainMenu, ...restaurant.starterMenu] 
+console.log(pizza, Risotto, otherfood)
+
+// Objects 
+const {sat, ...weekdays} = restaurant.openingHours
+console.log(weekdays)
+
+//2 
+
+const add = function(...numbers){
+let sum = 0;
+for(let i = 0; i < numbers.length; i++) sum += numbers
+[i];
+console.log(sum)
+};
+
+
+add(2, 3)
+add(5,3,7,2)
+add(8,2,5,3,2,1,4)
+
+const x = [23,5,7]
+add(...x)
+
+restaurant.orderPizza('mushroom', 'onion', 'olivers', 'spinach');
+
+restaurant.orderPizza('mushroom')
