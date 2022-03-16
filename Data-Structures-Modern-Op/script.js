e5233eda4bf55428dc78f312f8249f425716bd1b
Bryan`Y. Limz <superuser@brylimz.com>
2022-03-16 08:55:18 +0700

    refactor

--- Data-Structures-Modern-Op/script.js
+++ Data-Structures-Modern-Op/script.js
@@ -1,5 +1,8 @@
-'use strict';
 
+
+'use strict';
+//  DECONSTRUCT OPERATOR
+/*
 // Data needed for a later exercise
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
@@ -108,3 +111,4 @@ const [p, q, r] = [8, 9 ]
 console.log(p,q,r)
 
 */
+
