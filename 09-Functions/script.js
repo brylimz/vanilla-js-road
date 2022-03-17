e41eb630c869de905b470f0a92f735ef32a5b321
Bryan`Y. Limz <superuser@brylimz.com>
2022-03-17 18:23:32 +0700

    default paramater

--- 09-Functions/script.js
+++ 09-Functions/script.js
@@ -1,1 +1,22 @@
 'use strict';
+const bookings = []
+
+const createBooking = function(flightNum, 
+    numPassengers = 1,
+     price = 199 * numPassengers) {
+// numPassengers = numPassengers || 1;
+// price = price || 199;
+
+    const booking = {
+        flightNum,
+        numPassengers,
+        price
+    }
+    console.log(booking)
+    bookings.push(booking)
+}
+
+
+createBooking('LH123')
+createBooking('LH123', 2, 800)
+createBooking('LH123', undefined, 1000)
