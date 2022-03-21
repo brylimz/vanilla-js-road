d53458ba1d67a244d10d9f6400fe02640b62fd86
Bryan`Y. Limz <superuser@brylimz.com>
2022-03-21 12:05:03 +0700

    continues

--- 10-Bankist-Array/script.js
+++ 10-Bankist-Array/script.js
@@ -61,17 +61,47 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function(movements){
+movements.forEach(function(mov, i){
+const type = mov > 0 ? 'deposit' : 'withdrawal'
+
+const html=` 
+<div class="movements__row">
+  <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+  <div class="movements__value">${mov}}</div>
+</div>
+`
+
+.insertAdjacentHTML
+
+})
+}
+displayMovements(account1.movements)
+
+
+
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 
 currencies.forEach(function(value, key, map){
 console.log(`${key}: ${value}`);
@@ -81,12 +111,10 @@ console.log(`${key}: ${value}`);
 // set 
 const curreniesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR'])
 console.log(curreniesUnique)
-curreniesUnique.forEach(function(value,key,map) {
-  console.log(`${key}, ${value}`);
+curreniesUnique.forEach(function(value,_,map) {
+  console.log(`${value}, ${value}`);
 })
 
-
-/*
 //  for(const movement of movements) {
 for(const [i, movement] of movements.entries()){
 if(movement > 0) {
