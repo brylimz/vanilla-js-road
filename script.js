5ac89b4b0ebb0e27655fd1685afc970be456b1fe
Bryan`Y. Limz <superuser@brylimz.com>
2022-04-06 18:38:26 +0700

    nice

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 // change text-center text-white text-uppercase'
 
 const textCenter = document.querySelector('.text-center')
+
+
 const list1 = document.querySelector('.ml-2')
 // change text-center text-white text-uppercase
+
+const listGroup = document.querySelector('.list-group')
+const listGroupItem = document.querySelectorAll('.list-group-item')
+// change background color
+listGroup.style.backgroundColor = 'black'
+
+// change all background the listGroupItem  to black
+for (let i = 0; i < listGroupItem.length; i++) {
+    listGroupItem[i].style.backgroundColor = 'black'
+}
