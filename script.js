2c4c80255da7bbdf0882e1cb7e50d8a8dfe5a243
Bryan`Y. Limz <superuser@brylimz.com>
2022-04-06 18:22:26 +0700

    refactor

---
+++ script.js
@@ -0,0 +1,5 @@
+// change text-center text-white text-uppercase'
+
+const textCenter = document.querySelector('.text-center')
+const list1 = document.querySelector('.ml-2')
+// change text-center text-white text-uppercase
